e08e3a50558395f54327cbe59d529d1f138eff02
RandomTimeTV <dergepanserte@gmail.com>
2024-05-05 17:21:42 +0200

    WIP 0.6

---
+++ main.py
@@ -0,0 +1,15 @@
+from RTS_WebUIBuilder import RDocument, RWebserver, RLabel
+
+webs = RWebserver(host='0.0.0.0',port=8080)
+
+def main(webs):
+    print("Starting Webserver on port ", webs.port)
+
+    dom = RDocument(documentRoute="/killme")
+
+    dom.body.title = RLabel()
+    dom.body.title.displaytext = "Kill me already!"
+    dom.asm()
+
+webs.run(main, webs)
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RTS-WebUIBuilder',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     description='A simple webserver with a simple webui builder.',
     long_description=open('README.md').read(),
